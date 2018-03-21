8a3eb3acc9012a69afb14b1692e838d8b62aa167
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-03-21 12:25:41 +0000

    described blocks added to jasmine

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -4,62 +4,73 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
-  it("has a starting temperature of 20", function() {
-    expect(thermostat.temp).toEqual(20);
+  describe('starting temperature', function() {
+    it("has a starting temperature of 20", function() {
+      expect(thermostat.temp).toEqual(20);
+    });
   });
 
-  it("You can increase the temperature with an `up` function", function() {
-    thermostat.up(4);
-    expect(thermostat.temp).toEqual(24);
+  describe('#up', function() {
+    it("You can increase the temperature with an `up` function", function() {
+      thermostat.up(4);
+      expect(thermostat.temp).toEqual(24);
+    });
   });
 
-  it("You can decrease the temperature with a `down` function", function() {
-    thermostat.down(6);
-    expect(thermostat.temp).toEqual(14);
+  describe('#down', function() {
+    it("You can decrease the temperature with a `down` function", function() {
+      thermostat.down(6);
+      expect(thermostat.temp).toEqual(14);
+    });
+
+    it("You cant go below 10 degrees on the thermostat", function() {
+      expect(thermostat.down(15)).toEqual("Minimum temp reached");
+    });
   });
 
-  it("You cant go below 10 degrees on the thermostat", function() {
+  describe('#powerSaveMode', function() {
+    it("I can turn on power saving mode", function() {
+      thermostat.powerSaveMode(true)
+      expect(thermostat.powerSave).toEqual(true);
+    });
 
-    expect(thermostat.down(15)).toEqual("Minimum temp reached");
+    it("I can turn off power saving mode", function() {
+      thermostat.powerSaveMode(false)
+      expect(thermostat.powerSave).toEqual(false);
+    });
+
+    it("when powerSaveMode is on, max temp is 25 ", function() {
+      thermostat.powerSaveMode(true)
+      expect(thermostat.up(10)).toEqual("Max temp reached");
+    });
+
+    it("when powerSaveMode is of, max temp is 32 ", function() {
+      thermostat.powerSaveMode(false)
+      expect(thermostat.up(15)).toEqual("Max temp reached");
+    });
   });
 
-  it("I can turn on power saving mode", function() {
-    thermostat.powerSaveMode(true)
-    expect(thermostat.powerSave).toEqual(true);
+  describe('#reset', function() {
+    it("when reset button is pressed, temp is 20 ", function() {
+      thermostat.reset()
+      expect(thermostat.temp).toEqual(20);
+    });
   });
 
-  it("I can turn off power saving mode", function() {
-    thermostat.powerSaveMode(false)
-    expect(thermostat.powerSave).toEqual(false);
-  });
+  describe('energy usage property', function() {
+    it("has an energy usage state of 'low-usage' when <18", function() {
+      thermostat.down(5);
+      expect(thermostat.energyUsage).toEqual("low-usage");
+    });
 
-  it("when powerSaveMode is on, max temp is 25 ", function() {
-    thermostat.powerSaveMode(true)
-    expect(thermostat.up(10)).toEqual("Max temp reached");
-  });
+    it("has an energy usage state of 'medium-usage' when 19<temp<25", function() {
+      expect(thermostat.energyUsage).toEqual("medium-usage");
+    });
 
-  it("when powerSaveMode is of, max temp is 32 ", function() {
-    thermostat.powerSaveMode(false)
-    expect(thermostat.up(15)).toEqual("Max temp reached");
-  });
-
-  it("when reset button is pressed, temp is 20 ", function() {
-    thermostat.reset()
-    expect(thermostat.temp).toEqual(20);
-  });
-
-  it("has an energy usage state of 'low-usage' when <18", function() {
-    thermostat.down(5);
-    expect(thermostat.energyUsage).toEqual("low-usage");
-  });
-
-  it("has an energy usage state of 'medium-usage' when 19<temp<25", function() {
-    expect(thermostat.energyUsage).toEqual("medium-usage");
-  });
-
-  it("has an energy usage state of 'high-usage' when >=25", function() {
-    thermostat.up(5);
-    expect(thermostat.energyUsage).toEqual("high-usage");
+    it("has an energy usage state of 'high-usage' when >=25", function() {
+      thermostat.up(5);
+      expect(thermostat.energyUsage).toEqual("high-usage");
+    });
   });
 
 });
